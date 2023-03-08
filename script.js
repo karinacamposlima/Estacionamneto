a57623ae647451f080a59864c5a1eca3817f4c99
Karina Campos Lima <124939575+karinacamposlima@users.noreply.github.com>
2023-03-07 21:05:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const btncadastrar = document.querySelector('#btn-cadastrar');
+
+function cadastrar(event) {
+
+    let data = new Date();
+    let horas = data.getHours();
+    let minutos = data.getMinutes();
+
+    event.preventDefault();
+    const modelo = document.querySelector('#modelo');
+    const placa = document.querySelector('#placa');
+    if (modelo == '') {
+        alert('digite o modelo do veiculo');
+        return false;
+    }
+
+    if (placa.value == '') {
+        alert('digite a placa dp veiculo');
+        return false;
+    }
+
+    let linha = `    
+    <tr>
+                            <td>${modelo.value}</td>
+                            <td>${placa.value}</td>
+                            <td>${horas}:${minutos}</td>
+                            <td><button onclick="deletar(this.parentNode.parentNode)" class="btn btn-danger"><i class="fa fa-close"></i></button></td>
+                        </tr>
+    
+    `;
+    const lista = document.querySelector('#lista');
+
+    lista.innerHTML = lista.innerHTML + linha;
+    modelo.value = '';
+    placa.value = '';
+}
+
+btncadastrar.addEventListener('click', cadastrar);
+
+function deletar(registro){
+
+    Swal.fire({
+        title: 'Voce tem Certeza?',
+        text: "Voce nao e capaz de reverter isso!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim,quero apagar!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            registro.remove();
+          Swal.fire(
+            'Deletado!',
+            'Veiculo removido do patio.!',
+            'success'
+          )
+        }
+      })
+
+
+
+}
+
